10ad8aaffb2dbd228ba5dac946dd445ccca4eb35
Rickard von Essen <rickard.von.essen@izettle.com>
2018-01-09 20:52:06 +0100

    WIP

--- users/rickard/rickard.h
+++ users/rickard/rickard.h
@@ -6,5 +6,17 @@
 #define SS_IF_SHIFT(c, s) ((get_mods() & ((MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)))) ? SEND_STRING(c) : SEND_STRING(s))
 
 // void my_custom_function(void);
+// Compose key style international characters for use with US (ANSI) layout
+#define C_SE_AA
+#define C_SE_AE
+#define C_SE_OSLH
+
+enum custom_keycodes {
+  OS_LIN = 0,
+  OS_WIN,
+  OS_MAC,
+};
+
+void compose_int_char(int, int, int);
 
 #endif
