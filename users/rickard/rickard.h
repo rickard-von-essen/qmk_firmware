7c15f8d7723a7c5b261ea40f82b5d0d3fc28a3f2
Rickard von Essen <rickard.von.essen@gmail.com>
2018-11-07 07:20:14 +0100

    unicode åäö

--- users/rickard/rickard.h
+++ users/rickard/rickard.h
@@ -3,18 +3,8 @@
 
 #include "quantum.h"
 
-void compose_int_char(int, int, int);
+#define SS_IF_SHIFT(c, s) ((get_mods() & ((MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)))) ? SEND_STRING(c) : SEND_STRING(s))
 
-// Compose key style international characters for use with US (ANSI) layout
-#define C_SE_AA compose_int_char(
-#define C_SE_AE
-#define C_SE_OSLH
-
-enum custom_keycodes {
-  C_OS_LIN = 0,
-  C_OS_WIN,
-  C_OS_MAC,
-};
-
+// void my_custom_function(void);
 
 #endif
