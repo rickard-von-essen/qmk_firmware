7b208d89a493d8e73fab6993bf062dbba48c7637
Rickard von Essen <rickard.von.essen@gmail.com>
2025-08-28 10:08:24 +0200

    Preonic: Add Home Row Mod

--- keyboards/preonic/keymaps/rickard/keymap.c
+++ keyboards/preonic/keymaps/rickard/keymap.c
@@ -57,6 +57,17 @@ enum preonic_keycodes {
     RAISE,
 };
 
+// Define the mod-tap keys for the home row
+#define HOME_A LGUI_T(KC_A)  // A acts as GUI (Cmd/Win) when held
+#define HOME_S LALT_T(KC_S)  // S acts as Alt when held
+#define HOME_D LSFT_T(KC_D)  // D acts as Shift when held
+#define HOME_F LCTL_T(KC_F)  // F acts as Ctrl when held
+
+#define HOME_J RCTL_T(KC_J)  // J acts as Ctrl when held
+#define HOME_K RSFT_T(KC_K)  // K acts as Shift when held
+#define HOME_L RALT_T(KC_L)  // L acts as Alt when held
+#define HOME_SCLN RGUI_T(KC_SCLN)  // ; acts as GUI (Cmd/Win) when held
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -73,10 +84,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_BSPC,  \
+  KC_ESC,  HOME_A,  HOME_S,  HOME_D,  HOME_F,  KC_G,    KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_ENT,  \
   _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
